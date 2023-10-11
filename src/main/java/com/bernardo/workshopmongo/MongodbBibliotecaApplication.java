8b7641afc44c915f9607de642050c71e375e679e
Bernardo <bernardomerlo49@gmail.com>
2023-10-11 11:18:45 -0300

    first commit

---
+++ src/main/java/com/bernardo/workshopmongo/MongodbBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.bernardo.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbBibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbBibliotecaApplication.class, args);
+	}
+
+}
